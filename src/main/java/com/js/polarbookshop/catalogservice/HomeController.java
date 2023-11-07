9c64e0be961d6e1e476b03620e9beaa40839b639
Jason S <jesprl@hotmail.com>
2023-11-06 20:57:21 -0500

    initial commit - finished chapter 3

---
+++ src/main/java/com/js/polarbookshop/catalogservice/HomeController.java
@@ -0,0 +1,14 @@
+package com.js.polarbookshop.catalogservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String getGreeting() {
+        return "Welcome to the book catalog!";
+    }
+}
