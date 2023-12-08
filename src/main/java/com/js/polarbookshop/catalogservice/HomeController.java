fed9444b9a33b9cd852bb045e215ac48c39dc2a1
Jason S <jesprl@hotmail.com>
2023-12-07 20:38:59 -0500

    finished configuration

--- src/main/java/com/js/polarbookshop/catalogservice/HomeController.java
+++ src/main/java/com/js/polarbookshop/catalogservice/HomeController.java
@@ -1,5 +1,6 @@
 package com.js.polarbookshop.catalogservice;
 
+import com.js.polarbookshop.catalogservice.config.PolarProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,8 +8,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
+    private final PolarProperties polarProperties;
+
+    public HomeController(PolarProperties polarProperties){
+        this.polarProperties = polarProperties;
+    }
+
     @GetMapping("/")
     public String getGreeting() {
-        return "Welcome to the book catalog!";
+        return this.polarProperties.getGreeting();
     }
 }
