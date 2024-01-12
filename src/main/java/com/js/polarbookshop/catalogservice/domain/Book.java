d809215fa16d9a7b69ff3b152d71a171fc0487f8
Jason S <jesprl@hotmail.com>
2024-01-11 20:04:23 -0500

    prepping for first official build

--- src/main/java/com/js/polarbookshop/catalogservice/domain/Book.java
+++ src/main/java/com/js/polarbookshop/catalogservice/domain/Book.java
@@ -34,6 +34,8 @@ public record Book (
     @Positive(message = "The book price must be greater than zero.")
     Double price,
 
+    String publisher,
+
     @CreatedDate
     Instant createdDate,
 
@@ -44,7 +46,7 @@ public record Book (
     int version
     
 ){
-    public static Book of(String isbn, String title, String author, Double price) {
-        return new Book(null, isbn, title, author, price, null, null, 0);
+    public static Book of(String isbn, String title, String author, Double price, String publisher) {
+        return new Book(null, isbn, title, author, price, publisher, null, null, 0);
     }
 }
---
+++ src/main/resources/db/migration/V2__Add_publisher_column.sql
@@ -0,0 +1,2 @@
+ALTER TABLE book 
+ADD COLUMN publisher varchar(255) NOT NULL DEFAULT 'TBA';
