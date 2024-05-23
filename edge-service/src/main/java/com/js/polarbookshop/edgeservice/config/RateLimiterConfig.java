91871be42f128e69a0350ec64eb4381083af3d79
Jason S <jesprl@hotmail.com>
2024-05-22 20:58:52 -0400

    add authentication

--- edge-service/src/main/java/com/js/polarbookshop/edgeservice/config/RateLimiterConfig.java
+++ edge-service/src/main/java/com/js/polarbookshop/edgeservice/config/RateLimiterConfig.java
@@ -5,13 +5,16 @@ import reactor.core.publisher.Mono;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver; // Import the missing KeyResolver class
+import java.security.Principal;
+
 
 @Configuration
 public class RateLimiterConfig {
 
     @Bean
     public KeyResolver keyResolver(){
-        return exchange -> Mono.just("anonymous");
+        return exchange -> exchange.getPrincipal()
+            .map(Principal::getName)
+            .defaultIfEmpty("anonymous");
     }
-    
 }
